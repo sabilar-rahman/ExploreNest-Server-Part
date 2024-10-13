c9d36720e22a322d1844467098ce218bf4f2a610
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-13 12:19:29 +0600

    post interface

--- src/app/modules/post/post.interface.ts
+++ src/app/modules/post/post.interface.ts
@@ -1,63 +1,20 @@
-// import { Types, Document } from 'mongoose'
+import { Types } from 'mongoose'
+import { TComment } from '../comment/comment.interface'
 
-// export interface TComment {
-//     commenter: Types.ObjectId
-//   createdAt?: Date;
-// }
-
-// export interface TAuthor {
-//   _id: Types.ObjectId;
-//   email: string;
-// }
-
-// export interface TPost extends Document {
-//   _id: Types.ObjectId;
-//   title: string;
-//   content: string;
-//   image?: string[];
-//   tags: 'premium' | 'everyone';
-//   upVotes: number;
-//   downVotes: number;
-//   author: Types.ObjectId | TAuthor ;
-//   comments?: TComment[];
-//   createdAt?: Date;
-//   updatedAt?: Date;
-// }
-
-// export interface TPopulatedPost extends Omit<TPost, 'author'> {
-//   author: TAuthor
-// }
-
-import { Types } from "mongoose";
-
-export interface TComment {
-  commenter: Types.ObjectId;
-  content: string;
-  _id: string;
+interface Voter {
+  user: Types.ObjectId
+  vote: 'upvote' | 'downvote'
 }
 
-type TCategory =
-  | "adventure"
-  | "business trip"
-  | "exploration"
-  | "historical"
-  | "beach"
-  | "mountain";
-
-export interface TPost {
-  title: string;
-  content: string;
-  image?: string;
-  tags: "premium" | "everyone";
-  comments?: TComment[];
-  commentsCount?: number;
-  //   upVotes?: number;
-  //   downVotes?: number;
-  upVotes: Types.ObjectId[];
-  downVotes: Types.ObjectId[];
-
-  author: Types.ObjectId;
-  category: TCategory;
-
-  isActive: boolean
+export type TPost = {
+  author: Types.ObjectId
+  title: string
+  content?: string
+  category: string
+  image?: string
+  comments: [TComment]
+  upVotes: number
+  downVotes: number
+  voters: Voter[]
+  premium?: boolean
 }
