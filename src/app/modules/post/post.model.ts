eaf54c9f67ad719f326347c8cb8cf861e6ba9eb8
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-13 12:19:53 +0600

    post model

--- src/app/modules/post/post.model.ts
+++ src/app/modules/post/post.model.ts
@@ -1,52 +1,31 @@
-import mongoose, { Schema } from "mongoose";
-import { TComment, TPost } from "./post.interface";
+import mongoose, { Schema } from 'mongoose'
+import { TPost } from './post.interface'
 
-// Comment Schema
-const CommentSchema: Schema = new Schema<TComment>(
+const postSchema: Schema<TPost> = new mongoose.Schema(
   {
-    commenter: { type: Schema.Types.ObjectId, ref: "user", required: true },
-    content: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-const PostSchema: Schema = new Schema<TPost>(
-  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     title: { type: String, required: true },
     content: { type: String, required: true },
-    image: { type: String ,required: false },
-    tags: { type: String, enum: ["premium", "basic"], required: true },
-    // upVotes: { type: Number, default: 0 },
-    // downVotes: { type: Number, default: 0 },
-    upVotes: [{ type: Schema.Types.ObjectId, ref: "user", required: true }],
-    downVotes: [{ type: Schema.Types.ObjectId, ref: "user", required: true }],
-
-    comments: [CommentSchema],
-    commentsCount: { type: Number, default: 0 },
-    author: { type: Schema.Types.ObjectId, ref: "user", required: true },
-    category: {
-      type: String,
-      enum: [
-        "adventure",
-        "business trip",
-        "exploration",
-        "historical",
-        "beach",
-        "mountain",
-      ],
-      default: "adventure",
-    },
-
-    isActive: { type: Boolean, default: true },
+    category: { type: String },
+    image: String,
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+    voters: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        vote: { type: String, enum: ['upvote', 'downvote'] }, // stores 'upvote' or 'downvote'
+      },
+    ],
+    premium: { type: Boolean, default: false },
   },
   {
     timestamps: true,
-  }
-);
+  },
+)
 
-// Create the Post model
-const Post = mongoose.model<TPost>("Post", PostSchema);
-
-export default Post;
+export const Post = mongoose.model<TPost>('Post', postSchema)
