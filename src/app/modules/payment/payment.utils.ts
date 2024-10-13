ccd9eeaaacf772f417c7bc66689a5019ad80ed41
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-13 12:31:12 +0600

    payment utils

--- src/app/modules/payment/payment.utils.ts
+++ src/app/modules/payment/payment.utils.ts
@@ -1,53 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import dotenv from "dotenv";
-import axios from "axios";
-import config from "../../config";
+import axios from 'axios'
 
-
-
-dotenv.config();
-export const initiatePayment = async (paymentData:any) => {
-  try {
-    const response = await axios.post(process.env.PAYMENT_URL!, {
-      store_id: process.env.STORE_ID,
-      signature_key: process.env.SIGNATURE_KEY,
-      tran_id: paymentData.tran_id,
-      success_url: `${config.live_backend_url}/api/payment/confirmation?tran_id=${paymentData.tran_id}&status=success`,
-      fail_url: `${config.live_backend_url}/api/payment/confirmation?status=failed`,
-      cancel_url: `${config.client_live_url}`,
-      amount: paymentData.amount,
-      currency: "BDT",
-      desc: "Service Booking Payment",
-      cus_name: paymentData.customer.name,
-      cus_email: paymentData.customer.email,
-      cus_add1: paymentData.customer.address,
-      cus_city: paymentData.customer.city || "Dhaka",
-      cus_country: "Bangladesh",
-      cus_phone: paymentData.customer.phone,
-      type: "json",
-    });
-    const paymentUrl = response.data?.payment_url;
-
-    if (!paymentUrl) {
-      throw new Error("Payment URL not found in response");
-    }
-    console.log(response.data);
-    return { status: response.status, paymentUrl };
-  } catch (error) {
-    console.error("Payment initiation failed:", error);
-    throw new Error("Payment initiation failed");
-  }
-};
-
-export const verifyPament = async (tnxId: string) => {
-  const response = await axios.get(process.env.PAYMENT_VERIFY_URL!, {
-    params: {
-      store_id: process.env.STORE_ID,
-      signature_key: process.env.SIGNATURE_KEY,
-      type: "json",
-      request_id: tnxId,
+const verifyPayment = async (transactionId: string) => {
+  const response = await axios.get(
+    `https://sandbox.aamarpay.com/api/v1/trxcheck/request.php`,
+    {
+      params: {
+        store_id: 'aamarpaytest',
+        signature_key: 'dbb74894e82415a2f7ff0ec3a97e4183',
+        type: 'json',
+        request_id: transactionId,
+      },
     },
-  });
+  )
+  return response?.data
+}
 
-  return response.data;
-};
+export default verifyPayment
