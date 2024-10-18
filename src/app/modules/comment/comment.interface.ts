2c125b53cbed71dd3d9e30d263d956ab86e494bb
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-19 05:42:12 +0600

    comment interface and model

--- src/app/modules/comment/comment.interface.ts
+++ src/app/modules/comment/comment.interface.ts
@@ -1,13 +1,29 @@
-import { Types } from 'mongoose'
+// import { Types } from 'mongoose'
+
+// export type TComment = {
+//   content: string
+//   author: Types.ObjectId
+//   postId: Types.ObjectId
+// }
+
+// import {Types} from "mongoose";
+
+// export type TComment = {
+//   _id?: string;
+//   post: Types.ObjectId;
+//   commenter: Types.ObjectId;
+//   comment: string;
+// };
+
+
+import mongoose from "mongoose";
 
 export type TComment = {
-  content: string
-  author: Types.ObjectId
-  postId: Types.ObjectId
-}
-
-
-
+  _id?: string;
+  post: mongoose.Types.ObjectId;
+  commenter: mongoose.Types.ObjectId;
+  comment: string;
+};
 
 
 
--- src/app/modules/comment/comment.model.ts
+++ src/app/modules/comment/comment.model.ts
@@ -1,21 +1,50 @@
-import mongoose, { Schema } from 'mongoose'
-import { TComment } from './comment.interface'
+// import mongoose, { Schema } from "mongoose";
+// import { TComment } from "./comment.interface";
+
+// const commentSchema = new Schema<TComment>(
+//   {
+//     commenter: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     post: { type: mongoose.Schema.Types, ref: "Post", required: true },
+
+//     // postId: {
+//     //   type: mongoose.Schema.Types.ObjectId,
+//     //   ref: 'Post',
+//     //   required: true,
+//     // },
+
+//     comment: { type: String, required: true },
+//   },
+//   { timestamps: true }
+// );
+
+// export const Comment = mongoose.model<TComment>("Comment", commentSchema);
+
+
+import mongoose, { Schema } from "mongoose";
+import { TComment } from "./comment.interface";
 
 const commentSchema = new Schema<TComment>(
   {
-    content: { type: String, required: true },
-    author: {
+    commenter: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: "User",
       required: true,
     },
-    postId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Post',
-      required: true,
+    post: { 
+      type: mongoose.Schema.Types.ObjectId, // Correct the type here
+      ref: "Post", 
+      required: true 
+    },
+    comment: { 
+      type: String, 
+      required: true 
     },
   },
-  { timestamps: true },
-)
+  { timestamps: true }
+);
 
-export const Comment = mongoose.model<TComment>('Comment', commentSchema)
+export const Comment = mongoose.model<TComment>("Comment", commentSchema);
