6a586f42f6f616a593a0cdd56807e280f9669946
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-19 06:33:02 +0600

    comment api route

--- src/app/modules/comment/comment.route.ts
+++ src/app/modules/comment/comment.route.ts
@@ -7,7 +7,7 @@ import { USER_ROLE } from '../user/user.utils'
 const router = Router()
 
 router.post(
-  '/create',
+  '/create-comment',
   auth(USER_ROLE.admin, USER_ROLE.user),
   CommentController.createComment,
 )
@@ -16,9 +16,9 @@ router.post(
 router.get('/:id', CommentController.getCommentByPostId)
 
 // update comment
-router.put('/update/:commentId', CommentController.updateComment)
+router.put('/update/:id', CommentController.updateComment)
 
 // delete comment
-router.delete('/delete/:commentId/:postId', CommentController.deleteComment)
+router.delete('/:id', CommentController.deleteComment)
 
 export const CommentRoute = router
