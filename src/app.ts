0c0116531f4c25ead816c8256370c92204de4f1a
sabilar-rahman <sabilarrahman36@gmail.com>
2024-10-21 13:52:23 +0600

    app.ts update and remove static path dir

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,17 @@ import globalErrorHandler from "./app/middlewares/globalErrorhandelers";
 import notFound from "./app/middlewares/notFoundRoute";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+// import path from "path";
 const app: Application = express();
 
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, "../public")));
 
 // const app = express();
 
 // Add CORS middleware
 app.use(
   cors({
-    origin: [
-     
-      "http://localhost:3000",
-    ],
+    origin: ["http://localhost:3000"],
     credentials: true,
     // Allow cookies, authorization headers with the same origin, and credentials
   })
@@ -28,8 +25,7 @@ app.use(express.json()); // This will parse incoming JSON requests
 app.use(cookieParser());
 
 // Add EJS view engine
-app.set("view engine", "ejs");
-
+// app.set("view engine", "ejs");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello!, This is Explore Nest server.");
